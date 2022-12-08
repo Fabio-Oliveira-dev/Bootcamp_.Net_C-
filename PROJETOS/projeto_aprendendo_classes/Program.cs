fd4fa67b5434b3d734fb631530be4f20d9295cf7
Fabio-Oliveira-Dev <fabiooliveiraideia2@gmail.com>
2022-12-08 15:56:04 -0300

    Primeiro projeto classes

---
+++ PROJETOS/projeto_aprendendo_classes/Program.cs
@@ -0,0 +1,7 @@
+﻿using PROJETOS.models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Fabio";
+pessoa1.Idade = 29;
+pessoa1.Apresentar();
---
+++ PROJETOS/projeto_aprendendo_classes/models/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PROJETOS.models
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá Meu nome é {Nome} e eu tenho {Idade} anos.");
+        }
+    }
+}
